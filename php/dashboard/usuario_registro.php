3f9a73c3286d97d1ba76f1848edae1a3160b1a01
Gabriel Ojitos Landa <44986617+ojitoslanda@users.noreply.github.com>
2025-10-23 15:23:59 -0500

    Update usuario_registro.php

--- php/dashboard/usuario_registro.php
+++ php/dashboard/usuario_registro.php
@@ -8,8 +8,8 @@ $sql = "INSERT INTO usuarios(nombre,correo,pass,roles)
         values('$nombre','$correo','$pass','$rol')";
 $resultado = $conexion->query($sql);  
 if($resultado){
-    header("Location ../../dash_usuario_registro");
+    header("Location: ../../dash_usuario_registro.php");
 }  else{
-    header("Location ../../dash_usuario_registro");
+    header("Location: ../../dash_usuario_registro.php");
 } 
-        //Cierre ?> 
+//Cierre ?> 
